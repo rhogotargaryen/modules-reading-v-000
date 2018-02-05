adac7606b69528c1c7b683541e544bd5e8ada851
Derek Haller <dhaller.personal@gmail.com>
2018-02-05 01:51:20 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,11 @@
-require_relative './class_methods_module.rb'
-require_relative './dance_module.rb'
+require "fancy_dance.rb"
+
+
+class Dancer
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end
+class Kid
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end
